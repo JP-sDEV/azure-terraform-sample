80a403fa4da60cf551894c92b68f7bcce3f4c744
JP-sDEV <jon.pablo80@gmail.com>
2024-12-05 11:11:54 -0500

    build: add cd pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,45 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_container_group" "app_container" {
-  name                = "app-container"
-  location            = "East US"
-  resource_group_name = "my-resource-group"
-  os_type             = "Linux"
+# Create a Resource Group
+resource "azurerm_resource_group" "testing_rg" {
+  name     = "testing-resource-group"
+  location = "East US"
+}
 
-  container {
-    name   = "app-container"
-    image  = "<your-docker-image>"  # Replace with your Docker image name
-    cpu    = "1.0"
-    memory = "1.5"
+# Create an Azure Container Registry (ACR)
+resource "azurerm_container_registry" "myacr" {
+  name                     = "myacr"  # The actual Azure registry name (must be unique globally)
+  resource_group_name       = azurerm_resource_group.testing_rg.name
+  location                 = azurerm_resource_group.testing_rg.location
+  sku                       = "Basic"
+  admin_enabled            = true
+}
 
-    ports {
-      port     = 3000  # Assuming Express is listening on port 3000
-      protocol = "TCP"
-    }
+# Create an Azure App Service Plan
+resource "azurerm_app_service_plan" "myapp_service_plan" {
+  name                = "my-app-service-plan"
+  location            = azurerm_resource_group.testing_rg.location
+  resource_group_name = azurerm_resource_group.testing_rg.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
   }
+}
 
-  tags = {
-    environment = "dev"
+# Create an Azure Web App
+resource "azurerm_web_app" "azure_terraform_sample_app" {
+  name                = "azure-terraform-sample"
+  location            = azurerm_resource_group.testing_rg.location
+  resource_group_name = azurerm_resource_group.testing_rg.name
+  app_service_plan_id = azurerm_app_service_plan.myapp_service_plan.id
+  site_config {
+    linux_fx_version = "DOCKER|myacr.azurecr.io/myapp:latest"
   }
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.testing_rg.name
 }
